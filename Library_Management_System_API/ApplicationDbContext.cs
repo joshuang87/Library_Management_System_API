472b695dc3a4c276ceb24d156bb6004a48b11c04
joshuang87 <joshuang43@gmail.com>
2023-10-22 22:47:08 +0800

    Add Db Context

---
+++ Library_Management_System_API/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System_API
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
