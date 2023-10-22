070a2e379743d441cff55feb594b83ffc6bbfa55
joshuang87 <joshuang43@gmail.com>
2023-10-22 23:12:10 +0800

    Created Get All Books Api

--- Library_Management_System_API/Controllers/BooksController.cs
+++ Library_Management_System_API/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library_Management_System_API.Controllers
 {
@@ -7,10 +8,25 @@ namespace Library_Management_System_API.Controllers
 	[ApiController]
 	public class BooksController : ControllerBase
 	{
-		private ApplicationDbContext _dbContext;
+		private readonly ApplicationDbContext _dbContext;
 		public BooksController(ApplicationDbContext dbContext)
 		{
 			_dbContext = dbContext;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllBooks()
+		{
+			try
+			{
+				var books = await _dbContext.Books.ToListAsync();
+
+				return Ok(books);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 		}
 	}
 }
