6f35e9f94e1ef8b9f827ad39c8df9268b8998bf7
joshuang87 <joshuang43@gmail.com>
2023-10-22 22:39:36 +0800

    Created Book Model

---
+++ Library_Management_System_API/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Library_Management_System_API.Models
+{
+	public class Book
+	{
+        public string Book_Id { get; set; }
+        public string Book_Title { get; set; }
+        public string Author { get; set; }
+        public int Category_Id { get; set; }
+        public DateTime Created_Time { get; set; } = DateTime.Now;
+        public DateTime Updated_Time { get; set; } = DateTime.Now;
+    }
+}
