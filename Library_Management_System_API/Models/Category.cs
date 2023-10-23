90eb935f27215ad9c971a3d1a45ce65c59ff4756
joshuang87 <joshuang43@gmail.com>
2023-10-23 10:36:34 +0800

    Created Category Model

---
+++ Library_Management_System_API/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System_API.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category_Id { get; set; }
+
+        public string Category_Name { get; set; }
+    }
+}
